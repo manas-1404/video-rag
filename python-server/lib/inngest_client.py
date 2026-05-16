52f5a2e6ccc3537395234c7fad6b664ac851028c
manas-1404 <manas.gowda04@gmail.com>
2026-05-15 18:19:49 -0700

    updated inngest params in python worker

--- python-server/lib/inngest_client.py
+++ python-server/lib/inngest_client.py
@@ -10,7 +10,7 @@ if _missing:
 
 client = inngest.Inngest(
     app_id="video-rag",
-    api_key=os.environ["INNGEST_EVENT_KEY"],
+    event_key=os.environ["INNGEST_EVENT_KEY"],
     signing_key=os.environ["INNGEST_SIGNING_KEY"],
     is_production=os.environ.get("RAILWAY_ENVIRONMENT") == "production",
 )
